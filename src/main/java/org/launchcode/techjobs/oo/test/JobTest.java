8f83d6b128fa1af4e5ad6625798f24596375a531
Zachary <gray.zachary1994@gmail.com>
2023-02-13 10:52:32 -0600

    Finished part 4 of assignment 2.

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+import static org.junit.Assert.*;
+
+
 
 /**
  * Created by LaunchCode
@@ -9,4 +14,34 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId() {
+        Job aJob = new Job();
+
+        Job anotherJob = new Job();
+
+        assertNotEquals(aJob.getId(), anotherJob.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job aJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(aJob.getEmployer() instanceof Employer);
+        assertTrue(aJob.getLocation() instanceof Location);
+        assertTrue(aJob.getPositionType() instanceof PositionType);
+        assertTrue(aJob.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(aJob instanceof Job);
+        assertEquals(aJob.getName(), "Product tester");
+        assertEquals(aJob.getEmployer().toString(), "ACME");
+        assertEquals(aJob.getLocation().toString(), "Desert");
+        assertEquals(aJob.getPositionType().toString(), "Quality control");
+        assertEquals(aJob.getCoreCompetency().toString(),"Persistence");
+    }
+    @Test
+    public void testJobsForEquality(){
+        Job aJob = new Job("Name", new Employer("Employer"), new Location("Location"), new PositionType("Position"), new CoreCompetency("Competency"));
+
+        Job anotherJob = new Job("Name", new Employer("Employer"), new Location("Location"), new PositionType("Position"), new CoreCompetency("Competency"));
+        assertFalse(aJob.equals(anotherJob));
+    }
 }
